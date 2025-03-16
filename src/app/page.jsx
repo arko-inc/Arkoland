4dc2078fa8ff002f3d01ad2d8e7e73f83122d0fa
Arko <arkotesla@gmail.com>
2025-03-16 12:49:01 +0600

    Afeef!

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,10 +3,11 @@ export default function Home() {
   return (
    <>
    <div className="bg-black w-full h-screen">
-    {/* <Navbar/> */}
     <h1 className="text-white text-center text-6xl">Welcome <br />to <br /> Arkoland!!!!!!!!!</h1>
-    <h2 className="text-white text-center text-7xl"><span className="mr-6">Under construction </span><Wrench/></h2>
-    {/* <Footer/> */}
+    <div className="flex justify-center items-center text-white text-7xl mt-10">
+      <span className="mr-1">Under construction</span>
+      <Wrench size={64} /> {/* Adjust the size as needed */}
+    </div>
    </div>
    </>
   );
--- src/components/ClientWrapper.jsx
+++ src/components/ClientWrapper.jsx
@@ -1,32 +1,23 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import EntryAnimation from "@/components/EntryAnimation";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export default function ClientWrapper({ children }) {
-  const [showEntryAnimation, setShowEntryAnimation] = useState(false);
-
-  useEffect(() => {
-    // Check if the entry animation has already been shown
-    const hasShownAnimation = localStorage.getItem("entryAnimationShown");
-
-    if (!hasShownAnimation) {
-      setShowEntryAnimation(true);
-    }
-  }, []);
+  const [showEntryAnimation, setShowEntryAnimation] = useState(true);
 
   const handleAnimationComplete = () => {
-    setShowEntryAnimation(false);
-    localStorage.setItem("entryAnimationShown", "true"); // Store in localStorage
+    setShowEntryAnimation(false); // Hide the EntryAnimation component
   };
 
   return (
     <>
-      {showEntryAnimation ? (
+      {showEntryAnimation && (
         <EntryAnimation onComplete={handleAnimationComplete} />
-      ) : (
+      )}
+      {!showEntryAnimation && (
         <>
           <Navbar />
           {children}
