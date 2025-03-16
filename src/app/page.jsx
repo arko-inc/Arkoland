1461f156e0e3fa780e930cec3a0882a209612723
Arko <arkotesla@gmail.com>
2025-03-16 10:25:46 +0600

    Fourth commit Navbar set

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,10 @@
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
   return (
    <>
    <div className="bg-black w-full h-screen">
+    <Navbar/>
     <h1 className="text-white text-center text-6xl">Hello Arkoland</h1>
    </div>
    </>
