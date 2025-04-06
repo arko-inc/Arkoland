bfea74d21fd3979759a96278f78c16d6c0b92910
Arko <arkotesla@gmail.com>
2025-04-07 04:01:11 +0600

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,15 +3,19 @@ import "./globals.css";
 
 export const metadata = {
   title: "Arkoland | The island of Arko's Awesome Ideas",
-  description: "Arkoland | The island of Arko's Awesome Ideas ",
+  description: "Arkoland | The island of Arko's Awesome Ideas",
+  
+  // ✅ Google Site Verification
+  verification: {
+    google: "uDJjy109SQDM3v-k652XNiivRNZidmB8O0kRKHMfdgw",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-black text-zinc-300 antialiased ">
+      <body className="bg-black text-zinc-300 antialiased">
         <ClientWrapper>{children}</ClientWrapper>
-        
       </body>
     </html>
   );
